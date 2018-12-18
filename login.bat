e7c6ba67f645ab864228804eafc31aaca0dd7346
tsc2017 <tangshch@shanghaitech.edu.cn>
2018-12-18 18:07:40 +0800

    script for autologin

---
+++ login.bat
@@ -0,0 +1,8 @@
+rem Replace "admin" with your Windows user name, make sure you have a valid path to "pythonw.exe"
+start C:\Users\admin\AppData\Local\Programs\Python\Python37-32\pythonw.exe login.py
+
+rem An alternative that is not recommended:
+rem start C:\Windows\pyw.exe login.py
+
+rem Use the following line instead if your are using Anaconda
+rem start /min C:\Users\admin\Anaconda3\pythonw.exe login.py
